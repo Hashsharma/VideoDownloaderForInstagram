9e1397abdb0cf29e775c6ef8ebd3c7e3343d845a
fanlitao <fanlitao@baidu.com>
2017-06-22 21:29:06 +0800

    a

--- app/src/main/java/com/zxmark/videodownloader/MainActivity.java
+++ app/src/main/java/com/zxmark/videodownloader/MainActivity.java
@@ -25,6 +25,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
