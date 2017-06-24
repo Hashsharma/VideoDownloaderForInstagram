9267bed3fbf7fa9273a9085b66e7e61a462d1f9d
fanlitao <fanlitao@baidu.com>
2017-06-24 20:18:23 +0800

    a

--- app/src/main/java/com/zxmark/videodownloader/util/Globals.java
+++ app/src/main/java/com/zxmark/videodownloader/util/Globals.java
@@ -9,5 +9,5 @@ public class Globals {
 
     public static final String ACTION_CHANGE_LOCALE = "action_change_locale";
     public static final String EXTRAS = "extras";
-    public static final boolean TEST_FOR_GP  = true;
+    public static final boolean TEST_FOR_GP  = false;
 }
